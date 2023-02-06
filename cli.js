752ba74f155ccf5e59d1005408bd3975ba90517d
Kirsten <amayak@email.unc.edu>
2023-02-05 22:53:52 -0500

    Update cli.js
    Added ' to console.log

--- cli.js
+++ cli.js
@@ -16,7 +16,7 @@ console.log('Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME
  console.log('   -n, -s        Latitude: N positive; S negative.');
   console.log('  -e, -w        Longitude: E positive; W negative.');
   console.log('  -z            Time zone: uses tz.guess() from moment-timezone by default.');
-  console.log('  -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.);
+  console.log('  -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.');
   console.log('  -j            Echo pretty JSON from open-meteo API and exit.');
 
 }
