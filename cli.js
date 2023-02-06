6fd261baa79bec5c661cdb5f091bdbad1f084783
Kirsten <amayak@email.unc.edu>
2023-02-05 22:44:49 -0500

    Update cli.js

--- cli.js
+++ cli.js
@@ -24,13 +24,7 @@ process.exit(0);
 //const url= ('https://api.open-meteo.com/v1/forecast?latitude=35.875&longitude=-79&hourly=temperature_2m&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_hours&current_weather=true&timezone=America%2FNew_York');
 
 
-//fetch 
-// Make a request
-//'.com?latitude=' + latitude + '&longitude=' + 
-//const response = await fetch (args._[0]+'?'+args.d);
-const response = await fetch(https://api.open-meteo.com/v1/forecast?latitude=' + Latitude + '&longitude=' + Longitude + '&hourly=temperature_2m&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_hours&current_weather=true&timezone=' + timezone);
-// Get the data from the request
-const data = await response.json();
+
 
 console.log(data)
 // Latitude
@@ -67,7 +61,13 @@ if(args.j)
 {
 console.log(data);
 }
-
+//Fetch
+// Make a request
+//'.com?latitude=' + latitude + '&longitude=' + 
+//const response = await fetch (args._[0]+'?'+args.d);
+const response = await fetch(https://api.open-meteo.com/v1/forecast?latitude=' + Latitude + '&longitude=' + Longitude + '&hourly=temperature_2m&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_hours&current_weather=true&timezone=' + timezone);
+// Get the data from the request
+const data = await response.json();
 
 
 
