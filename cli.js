a448edabe6aa83b9632170185958eab690474fc6
Kirsten <amayak@email.unc.edu>
2023-02-05 22:50:59 -0500

    Update cli.js
    For some reason all of my errors occur right at console.log

--- cli.js
+++ cli.js
@@ -11,15 +11,14 @@ console.log(args)
 
 if (args.h)
 {
-console.log('Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
-    -h            Show this help message and exit.
-    -n, -s        Latitude: N positive; S negative.
-    -e, -w        Longitude: E positive; W negative.
-    -z            Time zone: uses tz.guess() from moment-timezone by default.
-    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
-    -j            Echo pretty JSON from open-meteo API and exit.
-')
-process.exit(0);
+console.log('Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE);
+ console.log('   -h            Show this help message and exit.');
+ console.log('   -n, -s        Latitude: N positive; S negative.');
+  console.log('  -e, -w        Longitude: E positive; W negative.');
+  console.log('  -z            Time zone: uses tz.guess() from moment-timezone by default.');
+  console.log('  -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.);
+  console.log('  -j            Echo pretty JSON from open-meteo API and exit.');
+
 }
 //const url= ('https://api.open-meteo.com/v1/forecast?latitude=35.875&longitude=-79&hourly=temperature_2m&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_hours&current_weather=true&timezone=America%2FNew_York');
 
